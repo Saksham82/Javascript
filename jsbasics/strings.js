8c4499506409688673b8096d44fc470e34111a1f
Saksham <96918972+Saksham82@users.noreply.github.com>
2024-05-29 08:39:47 +0000

    .

---
+++ jsbasics/strings.js
@@ -0,0 +1,26 @@
+const name = "saksham"
+const repoCount = 19
+
+console.log(`Hello My name is ${name} and my repositories count is ${repoCount}`)
+
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.charAt('5'))
+// console.log(name.indexOf('m'))
+
+// const newName = name.substring(0,4)
+// console.log(newName)
+
+// const anotherName = name.slice(-5, 6)
+// console.log(anotherName)
+
+const newString1 = "   saksham   "
+console.log(newString1)
+console.log(newString1.trim())
+
+const url = "https://saksham.com/hitesh%20choudhary"
+console.log(url.replace('%20', '-'))
+
+//convert string to array 
+const name1 = "s a k s h a m"
+console.log(name1.split(' '))
